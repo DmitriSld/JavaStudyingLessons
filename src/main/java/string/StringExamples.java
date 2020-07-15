cbd8e46cd614e80878426337cde6b413347ce2ce
sadaxary <sadaxary@gmail.com>
2020-07-15 17:49:10 +0300

    csvprinter commit

--- src/main/java/string/StringExamples.java
+++ src/main/java/string/StringExamples.java
@@ -1,5 +1,24 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class StringExamples {
     Integer b;
+
+    public static void main(String[] args) {
+
+
+        Exception exception = new Exception();
+
+
+        ArrayList<String> strings = new ArrayList<String>();
+
+        Iterator<String> iterator = strings.iterator();
+
+        iterator.next();
+
+        "".toString();
+
+    }
 }
