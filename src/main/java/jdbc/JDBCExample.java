2b740da612a54a83d7bf4f165e501bbec53c5e61
sadaxary <sadaxary@gmail.com>
2020-07-17 23:11:32 +0300

    sql connection testing #1

---
+++ src/main/java/jdbc/JDBCExample.java
@@ -0,0 +1,19 @@
+package jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCExample {
+    public static void main(String[] args) {
+//        try (Connection connection = DriverManager.getConnection()) {
+//
+//        } catch (SQLException e) {
+//
+//        }
+
+
+        System.out.println("MySQL JDBC Connection Testing ~");
+        List<String> list = new ArrayList<>();
+        String SQL_SELECT = "Select * from list";
+    }
+}
